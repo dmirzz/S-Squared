0305463ab280a5ac8c779ff0ebf068abc5aeb9e5
dmirzz <154740657+dmirzz@users.noreply.github.com>
2024-04-22 00:58:34 +0500

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+console.log("This is the console for the website.");
+
+function showOverlay() {
+  document.getElementById("overlay").style.display = "flex";
+}
+
+function hideOverlay() {
+  document.getElementById("overlay").style.display = "none";
+}
